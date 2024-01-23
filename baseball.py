85ed8b813c18806e47f36649dcc1f5dfc9c3431f
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-01-23 08:14:22 -0800

    Created stlbounds.py

--- baseball.py
+++ baseball.py
@@ -12,8 +12,9 @@ f &= slab(x0=-2*R, x1=0)
 
 #f.save('baseball.stl')
 
-g = s & double_cone().rotate(pi /2, X).k(SMOOTH)
+g = s & (double_cone().rotate(pi /2, X).k(SMOOTH))
 
 baseball = f | g
 
-baseball.save('baseball.stl', samples=256**3)
+#baseball.save('baseball.stl', samples=256**3)
+baseball.save('baseball.stl', step=(0.2, 0.2, 0.2), bounds=((-30,-30,-30),(30,30,30)))

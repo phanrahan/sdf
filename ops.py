5afb92275ccb8491155f9b724850211e1f1c40c7
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-01-23 13:06:34 -0800

    More pattern generation tests

--- ops.py
+++ ops.py
@@ -8,6 +8,10 @@ from quadrics import double_cone
 _min = np.minimum
 _max = np.maximum
 _abs = np.abs
+_dot = np.dot
+
+def _normalize(a):
+    return a / np.linalg.norm(a)
 
 def _length(a):
     return np.linalg.norm(a, axis=1)
@@ -15,13 +19,50 @@ def _length(a):
 def _vec(*arrs):
     return np.stack(arrs, axis=-1)
 
+@sdf3
+def flatplane(normal=UP, point=ORIGIN):
+    normal = _normalize(normal)
+    def f(p):
+        return _abs(_dot(point - p, normal))
+    return f
+
 # first object gets a v-shaped engraving where it intersect the second
 @op3
-def engrave(a, b, r):
+def vgroove(a, b, r):
     def f(p):
         d1 = a(p)
         d2 = b(p)
-        return _max(d1, (d1 + r - abs(d2))*sqrt(0.5))
+        return _max(d1, (d1 + r - _abs(d2))*sqrt(0.5))
+    return f
+
+# first object gets a v-shaped engraving where it intersect the second
+@op3
+def vemboss(a, b, r):
+    def f(p):
+        d1 = a(p)
+        d2 = b(p)
+        return _min(d1, (d1 - r + _abs(d2))*sqrt(0.5))
+    return f
+
+# first object gets a circula-shaped engraving where it intersect the second
+@op3
+def cgroove(a, b, r):
+    def f(p):
+        d1 = a(p)
+        d2 = b(p)
+        d = _length(_vec(d1,d2)) - r
+        return _max(d1, -d)
+    return f
+
+# form a curve of radius r at the intersection of the two surfaces
+@op3
+def pipe(a, b, r):
+    def f(p):
+        d1 = a(p)
+        d2 = b(p)
+        print("d1 = ", d1.shape())
+        print("d2 = ", d2.shape())
+        return _length(_vec(d1,d2)) - r
     return f
 
 @op3
@@ -44,3 +85,18 @@ def union_round(a, b, r):
         return _max(r, _min (d1, d2)) - _length(u)
     return f
 
+
+@op3
+def circular_array(other, count, offset=0):
+    other = other.translate(X * offset)
+    da = np.pi / count
+    def f(p):
+        x = p[:,0]
+        y = p[:,1]
+        z = p[:,2]
+        d = np.hypot(x, y)
+        a = np.arctan2(y, x) % da
+        d1 = other(_vec(np.cos(a - da) * d, np.sin(a - da) * d, z))
+        d2 = other(_vec(np.cos(a) * d, np.sin(a) * d, z))
+        return _min(d1, d2)
+    return f
--- sphere.py
+++ sphere.py
@@ -2,7 +2,8 @@ from math import sqrt
 import numpy as np
 from sdf import *
 from quadrics import double_cone
-from ops import engrave
+from ops import flatplane, cgroove, vgroove, vemboss, pipe
+from ops import circular_array as circular
 
 _min = np.minimum
 _max = np.maximum
@@ -10,12 +11,48 @@ _abs = np.abs
 
 R = 25
 S = 1.2*R
-
-grooves = plane(Z).repeat((0,0,5))
+G = 0.2
 
 s = sphere(R)
-s = s.engrave(grooves,.2)
-s = s.engrave(grooves.rotate(pi/3,X),.2)
-s = s.engrave(grooves.rotate(pi/3,Y),.2)
+
+# doesn't work
+#pattern = circular(flatplane(X),10)
+
+# doesn't work
+#pattern = flatplane(X).circular_array(10)
+
+n = 10
+pattern = flatplane(X)
+for i in range(1,n):
+    pattern |= pattern.rotate(i*pi/n, Z)
+s = s.vemboss(pattern,G)
+
+#pattern = flatplane(Z).repeat((0,0,5))
+
+#s = s.vgroove(pattern,G)
+#s = s.vgroove(pattern.rotate(pi/2,X),G)
+#s = s.vgroove(pattern.rotate(pi/2,Y),G)
+
+#s = s.vemboss(pattern,G)
+#s = s.vemboss(pattern.rotate(pi/2,X),G)
+#s = s.vemboss(pattern.rotate(pi/2,Y),G)
+
+#s = s.vgroove(pattern,G)
+#s = s.vgroove(pattern.rotate(pi/3,X),G)
+#s = s.vgroove(pattern.rotate(pi/3,Y),G)
+
+#s = s.vemboss(pattern,G)
+#s = s.vemboss(pattern.rotate(pi/3,X),G)
+#s = s.vemboss(pattern.rotate(pi/3,Y),G)
+
+# doesn't work
+#s = s.cgroove(pattern,G)
+#s = s.cgroove(pattern.rotate(pi/3,X),G)
+#s = s.cgroove(pattern.rotate(pi/3,Y),G)
+
+# doesn't work - fails in _vec
+#s = s.pipe(pattern,G)
+#s = s.pipe(pattern.rotate(pi/3,X),G)
+#s = s.pipe(pattern.rotate(pi/3,Y),G)
 
 s.save('sphere.stl', step=0.2, bounds=((-S, -S, -S), (S, S, S)))

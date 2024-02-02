28c357b905f9b89097d63d824df0d2a34653ab05
phanrahan <myemail@stanford.edu>
2024-02-01 16:13:50 -0800

    Converted ornament to use fire

--- ornament.py
+++ ornament.py
@@ -1,74 +1,67 @@
-import sys
+import fire
 from math import pi
 from sdf import plane, slab, sphere, rectangle, X, Y, Z
 from quadrics import double_cone
 from ops import fan, slices, groove
 
-R = 25.
-SHELL = .15*R
-SMOOTH = 1.
-G = 0.25
-N = 24
 
-GROVE = rectangle(G).rotate(pi/4)
+class App(object):
+    def __init__(self, r=25, n=24, g=.25):
+        self.R = r
+        self.N = n
+        self.G = g
+        self.SHELL = .15
+        self.SMOOTH = 1.
 
+        self.s = sphere(self.R)
+        self.groove =  rectangle(self.G).rotate(pi/4)
 
-def diamond(s):
-    f = fan(N)
-    diag1 = f.twist( pi).scale(R)
-    diag2 = f.twist(-pi).scale(R)
-    s = groove(s, diag1.orient(X), GROVE)
-    s = groove(s, diag2.orient(X), GROVE)
-    return s
+    def ball(self):
+        self.name = 'ball'
 
-def triangle(s):
-    s = diamond(s)
-    s = groove(s, fan(2*N).orient(X), GROVE)
-    return s
+    def diamond(self):
+        f = fan(self.N)
+        diag1 = f.twist( pi).scale(self.R)
+        diag2 = f.twist(-pi).scale(self.R)
+        self.s = groove(self.s, diag1.orient(X), self.groove)
+        self.s = groove(self.s, diag2.orient(X), self.groove)
+        self.name = 'diamond'
 
-# not working ...
-def hex(s):
-    f = fan(N)
-    diag1 = f.twist( pi/2).scale(R)
-    diag2 = f.twist(-pi/2).scale(R)
-    s = groove(s, diag1.orient(X), GROVE)
-    s = groove(s, diag2.orient(X), GROVE)
-    s = groove(s, f.orient(X), GROVE)
-    #s = groove(s, fan(N).rotate(pi/(2*N), Z).orient(X), GROVE)
-    return s
+    def triangle(self):
+        self.diamond()
+        self.s = groove(self.s, fan(2*self.N).orient(X), self.groove)
+        self.name = 'triangle'
 
+    def hex(self):
+        f = fan(self.N)
+        diag1 = f.twist( pi/2).scale(self.R)
+        diag2 = f.twist(-pi/2).scale(self.R)
+        self.s = groove(self.s, diag1.orient(X), self.groove)
+        self.s = groove(self.s, diag2.orient(X), self.groove)
+        self.s = groove(self.s, f.orient(X), self.groove)
+        self.name = 'hex'
 
-def baseball(s):
-    # subtract an inner sphere to form shell
-    s = s - sphere(R-SHELL)
+    def baseball(self):
+        # subtract an inner sphere to form shell
+        self.s = self.s - sphere((1-self.SHELL)*self.R)
 
-    # remove a double cone oriented in Z, smooth the intersection
-    f = s - double_cone().k(SMOOTH)
-    # remove positive x halfspace
-    f &= slab(x0=-2*R, x1=0)
+        # remove a double cone oriented in Z, smooth the intersection
+        f = self.s - double_cone().k(self.SMOOTH)
+        # remove positive x halfspace
+        f &= slab(x0=-2*self.R, x1=0)
 
-    # form a double cone shaped cap oriented along Y
-    g = s & (double_cone().orient(Y).k(SMOOTH))
+        # form a double cone shaped cap oriented along Y
+        g = self.s & (double_cone().orient(Y).k(self.SMOOTH))
 
-    # return the union of the two pieces
-    return f | g
+        # return the union of the two pieces
+        self.s = f | g
 
-s = sphere(R) 
+    def exit(self):
+        self.s = self.s.translate((0.05,0.05,0.05))
+        self.s.save(self.name + '.stl', step=(0.1, 0.1, 0.1), bounds=((-30,-30,-30),(30,30,30)))
 
-name = "ball"
-if len(sys.argv) > 1:
-    name = sys.argv[1]
-    if name == 'diamond':
-        s = diamond(s)
-    elif name == 'triangle':
-        s = triangle(s)
-    elif name == 'hex':
-        s = hex(s)
-    else:
-        print("usage: python ornament.py [diamond|triangle|hex]")
-        sys.exit(0)
-
-ornament = baseball(s)
-
-ornament = ornament.translate((0.05,0.05,0.05))
-ornament.save(name+'.stl', step=(0.1, 0.1, 0.1), bounds=((-30,-30,-30),(30,30,30)))
+if __name__ == '__main__':
+  app = App()
+  fire.Fire(app)
+  app.baseball()
+  app.exit()

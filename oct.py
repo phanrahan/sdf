b254a0771851dbef93159cfbf7ae0d6d97c1540f
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-02-06 11:57:18 -0800

    Add main to oct

--- oct.py
+++ oct.py
@@ -1,6 +1,6 @@
-from math import sqrt, pi
+from math import pi
 from np_sdf import cross
-from sdf import sphere, plane, rectangle, X, Y, Z
+from sdf import sphere, plane, rectangle
 from ops import surface, groove
 import sym
 
@@ -11,18 +11,19 @@ V3 = (0,0,1)
 def oct(f):
     return f.fold()
 
-R = 25
-G = 2
+if __name__ == '__main__':
+    R = 25
+    G = 2
 
-e1 = surface(plane(cross(V1, V2)))
-e2 = surface(plane(cross(V2, V3)))
-e3 = surface(plane(cross(V3, V1)))
-e = e1 | e3 | e2
-g = rectangle(G).rotate(pi/4)
-s = groove(sphere(R),e,g)
+    e1 = surface(plane(cross(V1, V2)))
+    e2 = surface(plane(cross(V2, V3)))
+    e3 = surface(plane(cross(V3, V1)))
+    e = e1 | e3 | e2
+    g = rectangle(G).rotate(pi/4)
+    s = groove(sphere(R),e,g)
 
-s = oct(s)
+    s = oct(s)
 
-D = R+5
-step = D/128
-s.translate((step/2,step/2,step/2)).save('oct.stl', step=step, bounds=((-D, -D, -D), (D, D, D)))
+    D = R+5
+    step = D/128
+    s.translate((step/2,step/2,step/2)).save('oct.stl', step=step, bounds=((-D, -D, -D), (D, D, D)))

5d58f058c4f801466d321e2e7e43c635197a3f16
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-02-06 11:58:50 -0800

    Added main to tet

--- tet.py
+++ tet.py
@@ -1,7 +1,6 @@
-from math import sqrt, pi
-import numpy as np
+from math import pi
 from np_sdf import cross
-from sdf import sphere, plane, rectangle, X, Y, Z
+from sdf import sphere, plane, rectangle
 from ops import surface, groove
 import sym
 
@@ -12,18 +11,19 @@ V3 = (-1, 1, 1)
 def tet(f):
     return f.fold(cross(V1,V2)).fold(cross(V2,V3)).fold(cross(V3,V1))
 
-R = 25
-G = 2
+if __name__ == '__main__':
+    R = 25
+    G = 2
 
-e1 = surface(plane(cross(V1, V2)))
-e2 = surface(plane(cross(V2, V3)))
-e3 = surface(plane(cross(V3, V1)))
-e = e1 | e3 | e2
-g = rectangle(G).rotate(pi/4)
-s = groove(sphere(R),e,g)
+    e1 = surface(plane(cross(V1, V2)))
+    e2 = surface(plane(cross(V2, V3)))
+    e3 = surface(plane(cross(V3, V1)))
+    e = e1 | e3 | e2
+    g = rectangle(G).rotate(pi/4)
+    s = groove(sphere(R),e,g)
 
-s = tet(s)
+    s = tet(s)
 
-D = R+5
-step = D/128
-s.translate((step/2,step/2,step/2)).save('tet.stl', step=step, bounds=((-D, -D, -D), (D, D, D)))
+    D = R+5
+    step = D/128
+    s.translate((step/2,step/2,step/2)).save('tet.stl', step=step, bounds=((-D, -D, -D), (D, D, D)))

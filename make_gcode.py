5677466c2917fa3cd3d0b7eee467174be6f71ead
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-07-20 11:28:39 -0700

    Update make_gcode

--- make_gcode.py
+++ make_gcode.py
@@ -9,7 +9,8 @@ config = ".".join([config_base, filament, model, "ini"])
 
 gcode_suffix = "bgcode" if model == 'mk4' else "gcode"
 
-for file in glob.glob("*.stl"):
+#for file in glob.glob("*.stl"):
+for file in ["dots.stl"]:
     base = file.removesuffix(".stl")
     stl = base + ".stl"
     gcode = ".".join([base, filament, model, gcode_suffix])

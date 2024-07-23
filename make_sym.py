1558bd95195de8f5870e68c9e5fd2eb796e1f54f
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-07-23 07:39:48 -0700

    Convert to fire

---
+++ make_sym.py
@@ -0,0 +1,61 @@
+import fire
+from math import pi, sqrt
+from np_sdf import vec3, cross
+from sdf import sphere, rectangle
+from ops import groove
+from sym import subdivide
+
+
+class App(object):
+    def __init__(self, r=25, g=1., n=0):
+        self.R = r
+        self.G = g
+        self.N = n
+
+        self.s = sphere(self.R)
+        self.g =  rectangle(self.G).rotate(pi/4)
+
+        self.name = None
+
+    def tet(self):
+        self.name = 'tet'
+        V1 = vec3( 1,-1, 1)
+        V2 = vec3( 1, 1,-1)
+        V3 = vec3(-1, 1, 1)
+        self.e = subdivide(V1, V2, V3, self.N)
+        self.e = self.e.fold(cross(V1,V2)).fold(cross(V2,V3)).fold(cross(V3,V1))
+
+    def oct(self):
+        self.name = 'oct'
+        V1 = vec3(1,0,0)
+        V2 = vec3(0,1,0)
+        V3 = vec3(0,0,1)
+        self.e = subdivide(V1, V2, V3, self.N)
+        self.e = self.e.fold()
+
+    def ico(self):
+        self.name = 'ico'
+        PHI = (1.+sqrt(5.))/2.
+        A = PHI / sqrt( 1. + PHI*PHI )
+        B = 1. / sqrt( 1. + PHI*PHI )
+        V1 = vec3(B,0,A)
+        V2 = vec3(A,B,0) 
+        V3 = vec3(0,A,B) 
+        self.e = subdivide(V1, V2, V3, self.N)
+        self.e = self.e.fold(cross(V1,V2)).fold(cross(V2,V3)).fold(cross(V3,V1)).fold()
+
+
+    def exit(self):
+        if self.name:
+
+            s = groove(self.s, self.e, self.g)
+
+            D = self.R+5
+            step = D/128
+            s.translate((step/2,step/2,step/2)).save(self.name + '.stl', step=step, bounds=((-D, -D, -D), (D, D, D)))
+
+if __name__ == '__main__':
+  app = App()
+  fire.Fire(app)
+  app.exit()
+
--- sym.py
+++ sym.py
@@ -3,6 +3,9 @@ from np_sdf import np, abs, min, vec3, dot, cross, normalize
 from ops import surface, groove
 from sdf import op3, plane, rectangle
 
+def unit_plane(n):
+    return plane(normalize(n))
+
 @op3
 def fold(other, n=None):
     if n is not None:
@@ -17,28 +20,24 @@ def fold(other, n=None):
         return other(p)
     return f
 
-def triangle(s,V1,V2,V3,G):
-    e1 = surface(plane(cross(V1, V2)))
-    e2 = surface(plane(cross(V2, V3)))
-    e3 = surface(plane(cross(V3, V1)))
-    e = e1 | e2 | e3
-    g = rectangle(G).rotate(pi/4)
-    return groove(s,e,g)
+def triangle(V1,V2,V3):
+    e1 = surface(unit_plane(cross(V1, V2)))
+    e2 = surface(unit_plane(cross(V2, V3)))
+    e3 = surface(unit_plane(cross(V3, V1)))
+    return e1 | e2 | e3
 
-def subdivide(s, V1, V2, V3, G, n):
-    #V1 = normalize(V1)
-    #V2 = normalize(V2)
-    #V3 = normalize(V3)
+def subdivide(V1, V2, V3, n):
+    V1 = normalize(V1)
+    V2 = normalize(V2)
+    V3 = normalize(V3)
     if n > 0:
         V12 = V1 + V2
         V23 = V2 + V3
         V31 = V3 + V1
-        s = subdivide(s,V12,V23,V31,G,n-1)
+        s = subdivide(V12,V23,V31,n-1)
         s = s.fold(cross(V12,V23)).fold(cross(V23,V31)).fold(cross(V31,V12))
-        #s = subdivide(s,V23,V31,V12,G,n-1)
-        #s = s.fold(cross(V23,V12)).fold(cross(V31,V23)).fold(cross(V12,V31))
     else:
-        s = triangle(s,V1,V2,V3,G)
+        s = triangle(V1,V2,V3)
     return s
 
 
